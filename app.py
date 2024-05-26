a3219ea3441ff71af8c54524194c4e792fe9037c
Joker8503 <144357453+Joker8503@users.noreply.github.com>
2024-05-26 14:18:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import streamlit as st
 from prediction_page import show_predict_page
-from explore_page import show_explore_page
 
 # Use Streamlit's sidebar to navigate between pages
-page = st.sidebar.selectbox("Make a prediction or Explore Stats:", ["Predict", "Explore"])
+page = st.sidebar.selectbox("Make a prediction or Explore Stats:", ["Predict"])
 
 if page == "Predict":
     show_predict_page()
-else:
-    show_explore_page()
 
 
